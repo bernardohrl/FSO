10a301365f3c8dd6dbdda17da2560981f02a2443
bernardohrl <bernardohrl.unb@gmail.com>
2017-09-13 22:01:13 -0300

    Message Queue Working

--- client.c
+++ client.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/msg.h>
+#include <string.h>
 
 #define MSG_SIZE 100
 #define FALSE 0
@@ -40,14 +42,37 @@ int main() {
     printf("Insira a mensagem: ");
     fgets(message, MSG_SIZE, stdin);
 
-    printf("%s", message);
+
+    key_t key = ftok("./Makefile", 'j');         // Gera key para acessar fila
+    int queueId = msgget(key, 0666 | IPC_CREAT);          // Gela fila com acesso apenas de leitura (0444)
+    if(queueId == -1) {
+        printf("\n\n\tERROR: QUEUE NOT CREATED\n\n");
+        return 0;
+    }
+
+    struct my_msgbuf message_send = {2};                                     //Numero abitrario
+    strncpy(message_send.message_text, message, sizeof(message)-1);
+
+    int result = msgsnd(queueId, &message_send, sizeof(message_send), 0);
+    printf("\n%d\n", result);
+    printf("id: %d\n", queueId);
+    // printf("id: %d\n", queueId);
+
+    // printf("mensagem: %s\n %ld\n", message_struct.message_text, message_struct.message_type);
 
   } else {
     // Espera o processo filho terminar (sincronização)
     waitpid(process_son, NULL, 0);
-
     transport_header();
 
+    key_t key = ftok("./Makefile", 'j');
+    int queueId = msgget(key, 0666 | IPC_CREAT);
+    struct my_msgbuf message_recived;
+
+    msgrcv(queueId, &message_recived, sizeof(message_recived), 2, 0);
+
+    printf("mensagem: %s\n", message_recived.message_text);
+    
   }
 
   return 0;
